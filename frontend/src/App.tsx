a6bf57603a1ee67f21509e5866ee3aafb1247689
anass bentaib <you@example.com>
2024-02-29 14:40:55 +0100

    create app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { About, Dashboard, Home, Projects, Signin, Signup } from "./pages";
+import { Header } from "./Components";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ frontend/src/pages/index.ts
@@ -0,0 +1,7 @@
+import Home from "./Home";
+import About from "./About";
+import Signin from "./Signin";
+import Signup from "./Signup";
+import Projects from "./Projects";
+import Dashboard from "./Dashboard";
+export { Home, Dashboard, About, Signin, Signup, Projects };
