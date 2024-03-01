8fcd2a337a892ea3a6fa100a55e2331494a86b1c
anass bentaib <you@example.com>
2024-03-01 09:59:36 +0100

    create signup methd

--- backend/index.js
+++ backend/index.js
@@ -2,16 +2,18 @@ import express from "express";
 import mongoose from "mongoose";
 import * as dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
-
+import authRoutes from "./routes/auth.route.js";
 dotenv.config();
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("MongoDb is connected"))
   .catch((err) => console.log(err));
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("Server listening on port 3000 !");
 });
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
