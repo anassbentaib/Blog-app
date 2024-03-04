3d11bd29a8e1cca226aec5467bf398dfa28a666c
anass bentaib <you@example.com>
2024-03-04 21:28:48 +0100

    deploy issues

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,46 @@
 import express from "express";
-import * as dotenv from "dotenv";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
-import connectDB from "./utils/database.js";
-import cookieParser from "cookie-parser";
 import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js";
+import cookieParser from "cookie-parser";
 import path from "path";
 
 dotenv.config();
 
+mongoose
+  .connect("mongodb+srv://mongodb_databases:HgTHRXOyOyjoMM8R@cluster0.hnxpqnw.mongodb.net/blog-app")
+  .then(() => {
+    console.log("MongoDb is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const __dirname = path.resolve();
+
 const app = express();
+
 app.use(express.json());
 app.use(cookieParser());
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000!");
+});
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
-const __dirname = path.resolve();
 
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
+
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
@@ -34,17 +50,3 @@ app.use((err, req, res, next) => {
     message,
   });
 });
-
-const PORT = process.env.PORT || 300;
-const startServer = async () => {
-  try {
-    connectDB(process.env.MONGO_URL);
-    app.listen(PORT, () =>
-      console.log(`server running on port : http://localhost:${PORT}`)
-    );
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-startServer();
